e5562eb0a2652070139fa92881cb8d82a61f0785
SFAHOSUNG <jude7916@sfa.co.kr>
2016-03-02 08:44:55 +0900

    - 2016.03.02 / HSLEE - Polygon Scanner Parameter UI Display Update

--- LWDicer/UI/FormLaserMaint.Designer.cs
+++ LWDicer/UI/FormLaserMaint.Designer.cs
@@ -29,8 +29,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLaserMaint));
+            Syncfusion.Windows.Forms.Grid.GridRangeStyle gridRangeStyle1 = new Syncfusion.Windows.Forms.Grid.GridRangeStyle();
+            Syncfusion.Windows.Forms.Grid.GridRangeStyle gridRangeStyle2 = new Syncfusion.Windows.Forms.Grid.GridRangeStyle();
+            Syncfusion.Windows.Forms.Grid.GridRangeStyle gridRangeStyle3 = new Syncfusion.Windows.Forms.Grid.GridRangeStyle();
+            Syncfusion.Windows.Forms.Grid.GridRangeStyle gridRangeStyle4 = new Syncfusion.Windows.Forms.Grid.GridRangeStyle();
             this.BtnExit = new Syncfusion.Windows.Forms.ButtonAdv();
             this.BtnNext = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.BtnConfigureSave = new System.Windows.Forms.Button();
+            this.GridConfigure = new Syncfusion.Windows.Forms.Grid.GridControl();
+            ((System.ComponentModel.ISupportInitialize)(this.GridConfigure)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnExit
@@ -51,6 +58,71 @@
             this.BtnNext.Text = "Next";
             this.BtnNext.Click += new System.EventHandler(this.BtnNext_Click);
             // 
+            // BtnConfigureSave
+            // 
+            this.BtnConfigureSave.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnConfigureSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BtnConfigureSave.Image = ((System.Drawing.Image)(resources.GetObject("BtnConfigureSave.Image")));
+            this.BtnConfigureSave.Location = new System.Drawing.Point(1125, 507);
+            this.BtnConfigureSave.Name = "BtnConfigureSave";
+            this.BtnConfigureSave.Size = new System.Drawing.Size(125, 73);
+            this.BtnConfigureSave.TabIndex = 12;
+            this.BtnConfigureSave.Text = "  Save";
+            this.BtnConfigureSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnConfigureSave.UseVisualStyleBackColor = true;
+            this.BtnConfigureSave.Click += new System.EventHandler(this.BtnConfigureSave_Click);
+            // 
+            // GridConfigure
+            // 
+            this.GridConfigure.ActivateCurrentCellBehavior = Syncfusion.Windows.Forms.Grid.GridCellActivateAction.None;
+            this.GridConfigure.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GridConfigure.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.GridConfigure.Location = new System.Drawing.Point(30, 24);
+            this.GridConfigure.Name = "GridConfigure";
+            gridRangeStyle1.Range = Syncfusion.Windows.Forms.Grid.GridRangeInfo.Table();
+            gridRangeStyle1.StyleInfo.Font.Bold = false;
+            gridRangeStyle1.StyleInfo.Font.Facename = "Tahoma";
+            gridRangeStyle1.StyleInfo.Font.Italic = false;
+            gridRangeStyle1.StyleInfo.Font.Size = 9F;
+            gridRangeStyle1.StyleInfo.Font.Strikeout = false;
+            gridRangeStyle1.StyleInfo.Font.Underline = false;
+            gridRangeStyle1.StyleInfo.Font.Unit = System.Drawing.GraphicsUnit.Point;
+            gridRangeStyle2.Range = Syncfusion.Windows.Forms.Grid.GridRangeInfo.Table();
+            gridRangeStyle2.StyleInfo.Font.Bold = false;
+            gridRangeStyle2.StyleInfo.Font.Facename = "Tahoma";
+            gridRangeStyle2.StyleInfo.Font.Italic = false;
+            gridRangeStyle2.StyleInfo.Font.Size = 9F;
+            gridRangeStyle2.StyleInfo.Font.Strikeout = false;
+            gridRangeStyle2.StyleInfo.Font.Underline = false;
+            gridRangeStyle2.StyleInfo.Font.Unit = System.Drawing.GraphicsUnit.Point;
+            gridRangeStyle3.Range = Syncfusion.Windows.Forms.Grid.GridRangeInfo.Table();
+            gridRangeStyle3.StyleInfo.Font.Bold = false;
+            gridRangeStyle3.StyleInfo.Font.Facename = "Tahoma";
+            gridRangeStyle3.StyleInfo.Font.Italic = false;
+            gridRangeStyle3.StyleInfo.Font.Size = 9F;
+            gridRangeStyle3.StyleInfo.Font.Strikeout = false;
+            gridRangeStyle3.StyleInfo.Font.Underline = false;
+            gridRangeStyle3.StyleInfo.Font.Unit = System.Drawing.GraphicsUnit.Point;
+            gridRangeStyle4.Range = Syncfusion.Windows.Forms.Grid.GridRangeInfo.Table();
+            gridRangeStyle4.StyleInfo.Font.Bold = false;
+            gridRangeStyle4.StyleInfo.Font.Facename = "Tahoma";
+            gridRangeStyle4.StyleInfo.Font.Italic = false;
+            gridRangeStyle4.StyleInfo.Font.Size = 9F;
+            gridRangeStyle4.StyleInfo.Font.Strikeout = false;
+            gridRangeStyle4.StyleInfo.Font.Underline = false;
+            gridRangeStyle4.StyleInfo.Font.Unit = System.Drawing.GraphicsUnit.Point;
+            this.GridConfigure.RangeStyles.AddRange(new Syncfusion.Windows.Forms.Grid.GridRangeStyle[] {
+            gridRangeStyle1,
+            gridRangeStyle2,
+            gridRangeStyle3,
+            gridRangeStyle4});
+            this.GridConfigure.SerializeCellsBehavior = Syncfusion.Windows.Forms.Grid.GridSerializeCellsBehavior.SerializeAsRangeStylesIntoCode;
+            this.GridConfigure.Size = new System.Drawing.Size(1034, 707);
+            this.GridConfigure.SmartSizeBox = false;
+            this.GridConfigure.TabIndex = 11;
+            this.GridConfigure.UseRightToLeftCompatibleTextBox = true;
+            this.GridConfigure.CellClick += new Syncfusion.Windows.Forms.Grid.GridCellClickEventHandler(this.GridConfigure_CellClick);
+            // 
             // FormLaserMaint
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -58,11 +130,15 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(1280, 754);
+            this.Controls.Add(this.BtnConfigureSave);
+            this.Controls.Add(this.GridConfigure);
             this.Controls.Add(this.BtnNext);
             this.Controls.Add(this.BtnExit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormLaserMaint";
             this.Text = "FormLaserMaint";
+            this.Load += new System.EventHandler(this.FormLaserMaint_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridConfigure)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -71,5 +147,7 @@
 
         private Syncfusion.Windows.Forms.ButtonAdv BtnExit;
         private Syncfusion.Windows.Forms.ButtonAdv BtnNext;
+        private System.Windows.Forms.Button BtnConfigureSave;
+        private Syncfusion.Windows.Forms.Grid.GridControl GridConfigure;
     }
 }
--- LWDicer/UI/FormLaserMaint.cs
+++ LWDicer/UI/FormLaserMaint.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Windows.Forms;
 using LWDicer.Control;
 
+using Syncfusion.Windows.Forms.Grid;
+using Syncfusion.Windows.Forms;
+
+using static LWDicer.Control.DEF_PolygonScanner;
+
 namespace LWDicer.UI
 {
     public partial class FormLaserMaint : Form
@@ -15,6 +20,8 @@ namespace LWDicer.UI
         private PageInfo PrevPage = null;
         private PageInfo NextPage = null;
         private BottomPageInfo NextBottomPage = null;
+
+        private CPolygonParameter m_Polygon = null;
 
         public void SetPrevPage(PageInfo page)
         {
@@ -63,6 +70,252 @@ namespace LWDicer.UI
 
             CMainFrame.MainFrame.MoveToPage(PrevPage);
             CMainFrame.MainFrame.MoveToBottomPage(NextBottomPage);
+        }
+
+        private void InitGrid()
+        {
+            int i = 0, j = 0;
+
+            // Cell Click 시 커서가 생성되지 않게함.
+            GridConfigure.ActivateCurrentCellBehavior = GridCellActivateAction.None;
+
+            // Header
+            GridConfigure.Properties.RowHeaders = true;
+            GridConfigure.Properties.ColHeaders = true;
+
+            // Column,Row 개수
+            GridConfigure.ColCount = 3;
+            GridConfigure.RowCount = 25;
+
+            // Column 가로 크기설정
+            GridConfigure.ColWidths.SetSize(0, 200);
+            GridConfigure.ColWidths.SetSize(1, 80);
+            GridConfigure.ColWidths.SetSize(2, 100);
+            GridConfigure.ColWidths.SetSize(3, 650);
+
+            for (i = 0; i < 26; i++)
+            {
+                GridConfigure.RowHeights[i] = 27;
+            }
+
+            for (i = 0; i < 25; i++)
+            {
+                GridConfigure[i + 1, 1].BackColor = Color.FromArgb(230, 210, 255);
+                GridConfigure[i + 1, 3].BackColor = Color.FromArgb(255, 230, 255);
+            }
+
+            // Text Display
+            GridConfigure[0, 0].Text = "Parameter";
+            GridConfigure[0, 1].Text = "Unit";
+            GridConfigure[0, 2].Text = "Data";
+            GridConfigure[0, 3].Text = "Description";
+
+            GridConfigure[1, 0].Text = "InScanResolution";
+            GridConfigure[2, 0].Text = "CrossScanResolution";
+            GridConfigure[3, 0].Text = "InScanOffset";
+            GridConfigure[4, 0].Text = "StopMotorBetweenJobs";
+            GridConfigure[5, 0].Text = "PixInvert";
+            GridConfigure[6, 0].Text = "JobStartBufferTime";
+            GridConfigure[7, 0].Text = "PrecedingBlankLines";
+            GridConfigure[8, 0].Text = "SeedClockFrequency";
+            GridConfigure[9, 0].Text = "RepetitionRate";
+            GridConfigure[10, 0].Text = "CrossScanEncoderResol";
+            GridConfigure[11, 0].Text = "CrossScanMaxAccel";
+            GridConfigure[12, 0].Text = "EnCarSig";
+            GridConfigure[13, 0].Text = "SwapCarSig";
+            GridConfigure[14, 0].Text = "InterLeaveRatio";
+            GridConfigure[15, 0].Text = "FacetFineDelayOffset0";
+            GridConfigure[16, 0].Text = "FacetFineDelayOffset1";
+            GridConfigure[17, 0].Text = "FacetFineDelayOffset2";
+            GridConfigure[18, 0].Text = "FacetFineDelayOffset3";
+            GridConfigure[19, 0].Text = "FacetFineDelayOffset4";
+            GridConfigure[20, 0].Text = "FacetFineDelayOffset5";
+            GridConfigure[21, 0].Text = "FacetFineDelayOffset6";
+            GridConfigure[22, 0].Text = "FacetFineDelayOffset7";
+            GridConfigure[23, 0].Text = "StartFacet";
+            GridConfigure[24, 0].Text = "AutoIncrementStartFacet";
+            GridConfigure[25, 0].Text = "MotorStableTime";
+
+            GridConfigure[1, 1].Text = "[m]";
+            GridConfigure[2, 1].Text = "[m]";
+            GridConfigure[3, 1].Text = "[m]";
+            GridConfigure[4, 1].Text = "[-]";
+            GridConfigure[5, 1].Text = "[-]";
+            GridConfigure[6, 1].Text = "[sec]";
+            GridConfigure[7, 1].Text = "[-]";
+            GridConfigure[8, 1].Text = "[Hz]";
+            GridConfigure[9, 1].Text = "[Hz]";
+            GridConfigure[10, 1].Text = "[m]";
+            GridConfigure[11, 1].Text = "[m/s^2]";
+            GridConfigure[12, 1].Text = "[-]";
+            GridConfigure[13, 1].Text = "[-]";
+            GridConfigure[14, 1].Text = "[-]";
+            GridConfigure[15, 1].Text = "[m]";
+            GridConfigure[16, 1].Text = "[m]";
+            GridConfigure[17, 1].Text = "[m]";
+            GridConfigure[18, 1].Text = "[m]";
+            GridConfigure[19, 1].Text = "[m]";
+            GridConfigure[20, 1].Text = "[m]";
+            GridConfigure[21, 1].Text = "[m]";
+            GridConfigure[22, 1].Text = "[m]";
+            GridConfigure[23, 1].Text = "[-]";
+            GridConfigure[24, 1].Text = "[-]";
+            GridConfigure[25, 1].Text = "[ms]";
+
+            GridConfigure[1, 3].Text = "[Job Settings] Scanline에서 이웃한 두 pixel 사이 X축 거리";
+            GridConfigure[2, 3].Text = "[Job Settings] 이웃한 두 Scanline 사이 Y축 거리";
+            GridConfigure[3, 3].Text = "[Job Settings] 모든 Scanline의 X축 시작 위치를 조정";
+            GridConfigure[4, 3].Text = "[Job Settings] Exposure 이후 polygonmirror 정지 여부 결정";
+            GridConfigure[5, 3].Text = "[Job Settings] LaserPulse Exposure 되는 Bitmap의 색상 선택";
+            GridConfigure[6, 3].Text = "[Job Settings] Bitmap Uploading 시, exposure 하기전 대기 시간";
+            GridConfigure[7, 3].Text = "[Job Settings] Stage 가속시의 충분한 Settle-time을 위한 Dummy scanline 수";
+            GridConfigure[8, 3].Text = "[Laser Configuration] Laser의 Seed Clock 주파수 설정";
+            GridConfigure[9, 3].Text = "[Laser Configuration] 가공에 적용할 펄스 반복률(REP_RATE) 설정";
+            GridConfigure[10, 3].Text = "[CrossScan Configuration] StageEncoder 분해능 값 설정";
+            GridConfigure[11, 3].Text = "[CrossScan Configuration] Stagestart-up 과정의 최대 가속도";
+            GridConfigure[12, 3].Text = "[CrossScan Configuration] Stage Control Encoder signal 출력 여부";
+            GridConfigure[13, 3].Text = "[CrossScan Configuration] Stage movement direction 선택";
+            GridConfigure[14, 3].Text = "[Head Configuration] FacetFineDelayOffset 자동 설정 기능";
+            GridConfigure[15, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[16, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[17, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[18, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[19, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[20, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[21, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[22, 3].Text = "[Head Configuration] 각 scanline의 시작 위치 값의 미세 조정";
+            GridConfigure[23, 3].Text = "[Head Configuration] exposure의 첫 scanline에 해당하는 facet 지정";
+            GridConfigure[24, 3].Text = "[Head Configuration] 새로운job started 마다 StartFacet 값 증가";
+            GridConfigure[25, 3].Text = "[Polygon motor Configuration] speed-up 이후 exposure 시작 이전에 spinning 안정화 대기 시간";
+
+
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 26; j++)
+                {
+                    // Font Style - Bold
+                    GridConfigure[j, i].Font.Bold = true;
+
+                    GridConfigure[j, i].VerticalAlignment = GridVerticalAlignment.Middle;
+
+                    if (i != 3)
+                    {
+                        GridConfigure[j, i].HorizontalAlignment = GridHorizontalAlignment.Center;
+                    }
+                }
+            }
+
+            GridConfigure.GridVisualStyles = GridVisualStyles.Office2007Blue;
+            GridConfigure.ResizeColsBehavior = 0;
+            GridConfigure.ResizeRowsBehavior = 0;
+
+            GetPolygonPara();
+
+            // Grid Display Update
+            GridConfigure.Refresh();
+        }
+
+        private CPolygonParameter GetPolygonPara()
+        {
+            return m_Polygon = CMainFrame.LWDicer.m_Scanner.GetPolygonPara();
+        }
+
+        private void UpdateScreen(CPolygonParameter m_PolygonPara)
+        {
+            // User Enable Para
+            GridConfigure[1, 2].Text = string.Format("{0:F6}", m_PolygonPara.InScanResolution);
+            GridConfigure[2, 2].Text = string.Format("{0:F6}", m_PolygonPara.CrossScanResolution);
+            GridConfigure[3, 2].Text = Convert.ToString(m_PolygonPara.InScanOffset);
+            GridConfigure[4, 2].Text = Convert.ToString(m_PolygonPara.StopMotorBetweenJobs);
+            GridConfigure[5, 2].Text = Convert.ToString(m_PolygonPara.PixInvert);
+            GridConfigure[6, 2].Text = Convert.ToString(m_PolygonPara.JobStartBufferTime);
+            GridConfigure[7, 2].Text = Convert.ToString(m_PolygonPara.PrecedingBlankLines);
+
+            GridConfigure[8, 2].Text = Convert.ToString(m_PolygonPara.SeedClockFrequency);
+            GridConfigure[9, 2].Text = Convert.ToString(m_PolygonPara.RepetitionRate);
+
+            GridConfigure[10, 2].Text = string.Format("{0:F7}", m_PolygonPara.CrossScanEncoderResol);
+            GridConfigure[11, 2].Text = Convert.ToString(m_PolygonPara.CrossScanMaxAccel);
+            GridConfigure[12, 2].Text = Convert.ToString(m_PolygonPara.EnCarSig);
+            GridConfigure[13, 2].Text = Convert.ToString(m_PolygonPara.SwapCarSig);
+
+            GridConfigure[14, 2].Text = Convert.ToString(m_PolygonPara.InterleaveRatio);
+            GridConfigure[15, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset0);
+            GridConfigure[16, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset1);
+            GridConfigure[17, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset2);
+            GridConfigure[18, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset3);
+            GridConfigure[19, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset4);
+            GridConfigure[20, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset5);
+            GridConfigure[21, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset6);
+            GridConfigure[22, 2].Text = Convert.ToString(m_PolygonPara.FacetFineDelayOffset7);
+            GridConfigure[23, 2].Text = Convert.ToString(m_PolygonPara.StartFacet);
+            GridConfigure[24, 2].Text = Convert.ToString(m_PolygonPara.AutoIncrementStartFacet);
+            GridConfigure[25, 2].Text = Convert.ToString(m_PolygonPara.MotorStableTime);
+        }
+
+        private void BtnConfigureSave_Click(object sender, EventArgs e)
+        {
+            CMainFrame.LWDicer.m_Scanner.SavePolygonPara();
+
+            GetPolygonPara();
+
+            UpdateScreen(m_Polygon);
+        }
+
+        private void FormLaserMaint_Load(object sender, EventArgs e)
+        {
+            InitGrid();
+
+            UpdateScreen(m_Polygon);
+        }
+
+        private void GridConfigure_CellClick(object sender, GridCellClickEventArgs e)
+        {
+            int nCol = 0, nRow = 0;
+            string StrCurrent = "", strModify = "";
+
+            nCol = e.ColIndex;
+            nRow = e.RowIndex;
+
+            if (nCol != 2 || nRow == 0)
+            {
+                return;
+            }
+
+            StrCurrent = GridConfigure[nRow, nCol].Text;
+
+            if (!CMainFrame.LWDicer.GetKeyPad(StrCurrent, out strModify))
+            {
+                return;
+            }
+
+            GridConfigure[nRow, nCol].Text = strModify;
+
+            if (nRow == 1) CMainFrame.LWDicer.m_Scanner.SetPixelGridX(Convert.ToDouble(strModify));
+            if (nRow == 2) CMainFrame.LWDicer.m_Scanner.SetPixelGridY(Convert.ToDouble(strModify));
+            if (nRow == 3) CMainFrame.LWDicer.m_Scanner.SetStartOffset(Convert.ToDouble(strModify));
+            if (nRow == 4) CMainFrame.LWDicer.m_Scanner.SetMotorBetweenJob(Convert.ToInt16(strModify));
+            if (nRow == 5) CMainFrame.LWDicer.m_Scanner.SetBitMapColor(Convert.ToInt16(strModify));
+            if (nRow == 6) CMainFrame.LWDicer.m_Scanner.SetBufferTime(Convert.ToInt16(strModify));
+            if (nRow == 7) CMainFrame.LWDicer.m_Scanner.SetDummyBlankLine(Convert.ToInt16(strModify));
+            if (nRow == 8) CMainFrame.LWDicer.m_Scanner.SetSeedClock(Convert.ToDouble(strModify));
+            if (nRow == 9) CMainFrame.LWDicer.m_Scanner.SetRepRate(Convert.ToDouble(strModify));
+            if (nRow == 10) CMainFrame.LWDicer.m_Scanner.SetEncoderResol(Convert.ToDouble(strModify));
+            if (nRow == 11) CMainFrame.LWDicer.m_Scanner.SetMaxAccel(Convert.ToDouble(strModify));
+            if (nRow == 12) CMainFrame.LWDicer.m_Scanner.SetEnCarSig(Convert.ToInt16(strModify));
+            if (nRow == 13) CMainFrame.LWDicer.m_Scanner.SetSwapCarSig(Convert.ToInt16(strModify));
+            if (nRow == 14) CMainFrame.LWDicer.m_Scanner.SetLeaveRatio(Convert.ToInt16(strModify));
+            if (nRow == 15) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet0);
+            if (nRow == 16) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet1);
+            if (nRow == 17) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet2);
+            if (nRow == 18) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet3);
+            if (nRow == 19) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet4);
+            if (nRow == 20) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet5);
+            if (nRow == 21) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet6);
+            if (nRow == 22) CMainFrame.LWDicer.m_Scanner.SetSuperSync(Convert.ToDouble(strModify), Facet7);
+            if (nRow == 23) CMainFrame.LWDicer.m_Scanner.SetStartFacet(Convert.ToInt16(strModify));
+            if (nRow == 24) CMainFrame.LWDicer.m_Scanner.SetAutoIncStartFacet(Convert.ToInt16(strModify));
+            if (nRow == 25) CMainFrame.LWDicer.m_Scanner.SetMotorStableTime(Convert.ToInt16(strModify));
         }
     }
 }
